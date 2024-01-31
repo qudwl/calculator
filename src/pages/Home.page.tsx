681303749888d6c719b4297dd53786a1b2946bd5
Byeongji Lim <byeongjilim@gmail.com>
2024-01-31 11:45:34 -0500

    Starting Grid

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,11 +1,28 @@
-import { Welcome } from '../components/Welcome/Welcome';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { ActionIcon, Button, Card, Container, Grid } from '@mantine/core';
+import {IconBackspace} from '@tabler/icons-react';
 
 export function HomePage() {
   return (
-    <>
-      <Welcome />
-      <ColorSchemeToggle />
-    </>
+    <Container size="xs" mt="lg">
+      <Card shadow="sm" padding="lg" radius="md" withBorder>
+        <Card.Section />
+          <Grid>
+            <Grid.Col span={3}>
+              <Button>%</Button>
+            </Grid.Col>
+            <Grid.Col span={3}>
+              <Button>CE</Button>
+            </Grid.Col>
+            <Grid.Col span={3}>
+              <Button>C</Button>
+            </Grid.Col>
+            <Grid.Col span={3}>
+              <ActionIcon variant="filled">
+                <IconBackspace />
+              </ActionIcon>
+            </Grid.Col>
+          </Grid>
+      </Card>
+    </Container>
   );
 }
